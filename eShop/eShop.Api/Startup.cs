c8a4f82db1ce9ca3861da32871dfec769e0d98b8
Radu Solca <radu.ssolca@gmail.com>
2018-07-16 17:51:20 +0300

    Strip the code to its bare bones.

--- eShop/eShop.Api/Startup.cs
+++ eShop/eShop.Api/Startup.cs
@@ -1,10 +1,5 @@
-﻿using eShop.DataAccess.EntityFramework;
-using eShop.DataAccess.EntityFramework.Context;
-using eShop.Domain;
-using eShop.Domain.Repositories;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
@@ -24,17 +19,6 @@ namespace eShop.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
-            // EF
-            services.AddDbContext<OnlineShopContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
-            services.AddTransient(typeof(IRepository<Customer>), typeof(EFCustomerRepository));
-            services.AddTransient(typeof(IRepository<Product>), typeof(EFProductRepository));
-            services.AddTransient(typeof(IRepository<Order>), typeof(EFOrderRepository));
-            services.AddTransient(typeof(IRepository<OrderItem>), typeof(EFOrderItemRepository));
-
-            // Dapper
-            // services.AddTransient(typeof(IRepository<Customer>), typeof(CustomerRepository));
-            // services.AddTransient(typeof(IRepository<Order>), typeof(OrderRepository));
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
